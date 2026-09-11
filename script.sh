d9ae56cc066303c2db9fa745daa4bd8130092769
Crisda <crisda100256@gmail.com>
2026-09-11 11:36:39 -0500

    added new line a script file

--- script.sh
+++ script.sh
@@ -4,3 +4,11 @@
 echo "From develop branch !!"
 
 
+for i in {1..10}; do echo $i; done 
+
+servers=("web01" "web02" "db01")
+
+for server in "${servers[@]}"; do
+    echo "Pinging $server..."
+done
+
